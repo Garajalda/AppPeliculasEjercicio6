4886ec22d3ea61c1f0d9d6f994cf5cee2803692c
Gara Jalda <ga.jalda99@gmail.com>
2021-11-02 19:35:30 +0100

    commit

--- app/src/main/java/com/gara/ejercicio6/MainActivity.java
+++ app/src/main/java/com/gara/ejercicio6/MainActivity.java
@@ -1,14 +1,29 @@
 package com.gara.ejercicio6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
